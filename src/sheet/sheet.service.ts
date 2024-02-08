0ef132158564430d7934a49685f94b458433f3ce
Gokcer Gokdal <gokcer@armikom.com>
2024-02-08 20:16:36 +0300

    Loading credentials from environment variables added.

--- src/sheet/sheet.service.ts
+++ src/sheet/sheet.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { GoogleAuth } from 'google-auth-library';
 import { google } from 'googleapis';
+import * as fs from 'fs';
 
 @Injectable()
 export class SheetService {
+  auth: GoogleAuth;
+  private service: any;
 
-  auth = new GoogleAuth({
-    scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    keyFile: 'credentials.json',
-  });
+  constructor() {
+    if (fs.existsSync('credentials.json')) {
+      this.auth = new GoogleAuth({
+        scopes: 'https://www.googleapis.com/auth/spreadsheets',
+        keyFile: 'credentials.json',
+      });
+    } else {
+      this.auth = new GoogleAuth({
+        scopes: 'https://www.googleapis.com/auth/spreadsheets',
+        credentials: {
+          client_email: process.env.GOOGLE_CLIENT_EMAIL,
+          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        },
+      });
+    }
 
-  service = google.sheets({ version: 'v4', auth: this.auth });
+    this.service = google.sheets({ version: 'v4', auth: this.auth });
+  }
 
   async readSheetData(sheetId: string): Promise<any[][]> {
     const res = await this.service.spreadsheets.values.get({
@@ -21,11 +36,8 @@ export class SheetService {
 
     if (rows != null && rows.length > 0) {
       return rows;
-
     } else {
       console.log('No data found.');
     }
   }
-  
-
 }
